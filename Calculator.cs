de19a2b0215e630431912140d101c79f9a7288df
Repsi0 <repeerc710@gmail.com>
2020-04-17 12:14:09 -0500

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -8,73 +8,65 @@ namespace Calculator
 {
 	class Calculator
 	{
+		static string[] ops = {"+","-","*","/"};
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Calculator");
-			double Ans;
-			string Op = "";
+			OperationType op = OperationType.NULL;
 
-			while (Op != "+" && Op != "-" && Op != "*" && Op != "/")
-			{
-				Console.WriteLine("Please choose an operation from the following list: +, -, *, / ");
-				Op = Console.ReadLine();
-			}
-
-			double num1 = double.PositiveInfinity;
-			while (num1 == double.PositiveInfinity)
-			{
-				try
-				{
-					Console.WriteLine("Please choose first number");
-					double number = double.Parse(Console.ReadLine());
-					num1 = number;
+			bool firstLoop = true;
+			do {
+				if(!firstLoop) Console.WriteLine("Invalid operation. Please try again");
+				Console.WriteLine("Please choose an operation from the following list: +, -, *, /");
+				int foundAt = Array.IndexOf(ops, Console.ReadLine());
+				if(foundAt != -1) op = (OperationType) (foundAt+1); // +1 to account for the NULL OperationType
+				firstLoop=false;
+			} while(op == OperationType.NULL);
+			firstLoop=true;
+			do {
+				if(!firstLoop) Console.WriteLine("That is not a number. Please try again");
+				Console.WriteLine("Please choose first number");
+				firstLoop=false;
+			} while (!Double.TryParse(Console.ReadLine(), out double num1));
+			firstLoop=true;
+			double num2;
+			bool success_parse,success_div0; success_parse=success_div0=true;
+			do {
+				if(!firstLoop) {
+					if(!success_parse) Console.WriteLine("That is not a number. Please try again");
+					if(!success_div0)  Console.WriteLine("Cannot divide by 0. Please try again");
 				}
-				catch
-				{
-					Console.WriteLine("That is not a number. Please try again");
-				}
-				
-			}
-
-			double num2 = double.PositiveInfinity;
-			while (num2 == double.PositiveInfinity)
-			{
-				try
-				{
-					Console.WriteLine("Please choose second number");
-					double number = double.Parse(Console.ReadLine());
-					num2 = number;
-				}
-				catch
-				{
-					Console.WriteLine("That is not a number. Please try again");
-				}
-				if(Op == "+")
-				{
-					Ans = num1 + num2;
-				}else
-					if(Op == "-")
-				{
-					Ans = num1 - num2;
-				}else
-					if(Op == "*")
-				{
-					Ans = num1 * num2;
-				}else
-					if(Op == "/")
-				{
-					Ans = num1 / num2;
-				}else
-				{
-					Console.WriteLine("ERROR! Incorrect Calculation Operator 00, Press any key to close");
-					Console.ReadKey();
-					return;
-				}
-				Console.WriteLine($"{num1} {Op} {num2} = {Ans}");
-			}
+				Console.WriteLine("Please choose second number");
+				firstLoop=false;
+				success_parse = Double.TryParse(Console.ReadLine(), out num2);
+				success_div0 = (num2==0&&op==OperationType.DIV);
+			} while (!success_parse || !success_div0);
+			
+			Console.WriteLine($"{num1} {Op} {num2} = {RunOperation(num1,Op,num2)}");
 
 			Console.WriteLine("Program finished. Press any key to close");
 			Console.ReadKey();
 		}
+		public static double RunOperation(double a, OperationType op, double b) {
+			switch(op) {
+				case OperationType.ADD:
+					return a+b;
+				case OperationType.SUB:
+					return a-b;
+				case OperationType.MUL;
+					return a*b;
+				case OperationType.DIV:
+					return a/b;
+				default:
+					throw new NotImplementedException($"The operation {op.ToString()} has not been implemented yet, sorry!");
+			}
+		}
+	}
+	enum OperationType {
+		NULL,
+		ADD,
+		SUB,
+		MUL,
+		DIV
 	}
 }
